4e10d4842fe0a50d271a01bc393676321ccee77b
leeloxs <leeloxs@gmail.com>
2021-03-26 11:40:00 +0800

    test commit

---
+++ resources/views/donate.blade.php
@@ -0,0 +1,54 @@
+
+
+        <!-- Navigation-->
+<link href="css/styles.css" rel="stylesheet" />
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="/img/logomakr1.png" style="width: 150px; height: 50px" /></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ml-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/index">Home</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/donate">Donate</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/community">Community</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/team">Team</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/contact2">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/login">Login</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/register">Register</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+   <!-- Services-->
+   <section class="page-section" id="services">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Our Services</h2>
+                    <h3 class="section-subheading text-muted">Be a part of the change!</h3>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                    <a href="/financial"><img src="/img/fin.png" alt="donate" style="width:140px;height:140px;">
+                        <h4 class="my-3" href="/financial" target="_blank">Financial Donation</h4> </a>
+                        <p class="text-muted">Your donation will be 100% given to local charities. Safe and Fast!
+                        
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                    <a href="default.asp"><img src="/img/drop.png" alt="donate" style="width:140px;height:140px;"></a>
+                        <h4 class="my-3">Item Donation</h4>
+                        <p class="text-muted">Got extra item that you don't need? Post the item here and donate it to those who need it the most!</p>
+                    </div>
+                    <div class="col-md-4">
+                    <a href="default.asp"><img src="/img/comm.png" alt="donate" style="width:140px;height:140px;"></a>
+                        <h4 class="my-3">Community</h4>
+                        <p class="text-muted">Connect with those around you and see what do they need the most!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
---
+++ routes/web.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MollieController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes ( pages link )
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/about', function () {
+    return view('index');
+});
+
+Route::get('contact', 'ContactController@create');
+
+Route::post('contact', 'ContactController@store');
+
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/community', function () {
+    return view('community');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/contact2', function () {
+    return view('contact2');
+});
+
+Route::get('/donate', function () {
+    return view('donate');
+});
+
+Route::get('/financial', function () {
+    return view('financial');
+});
+
+
+
+//Route::get('/profile', function () {
+  //  return view('profile');
+//});
+
+Route::view("contact",'contact'); 
+Route::view("profile",'profile'); 
+//Route:get('/contact',[ContactController::class,'contact']);   //contact-form & contact function 
+//Route::post('/send-message',[ContactController::class,'sendEmail'])->name(contact.send);
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// Payment Gateway method
+Route::get('mollie-payment',[MollieController::Class,'preparePayment'])->name('mollie.payment');
+Route::get('payment-success',[MollieController::Class, 'paymentSuccess'])->name('payment.success');
